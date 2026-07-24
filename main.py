4c4dbc58efa21162e6df15749d57cd43d72a7d16
nabaafatima <nabaafatima7097@gmail.com>
2026-07-24 17:27:24 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import board
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners.keypad import DirectPins
+keyboard = KMKKeyboard()
+keyboard.matrix = DirectPins(
+    pins = (board.D9, board.D11, board.10, board.8),
+    value_when_pressed = False,
+    pull= True,
+)
+keyboard.keymap = [
+    [
+        KC.macro(KC.LCTRL, KC.C)
+        KC.macro(KC.LCTRL, KC.V)
+        KC.macro(KC.LCTRL, KC.Z)
+        KC.macro(KC.LCTRL, KC.Y)
+    ]
+]
+if __name__ == '__main__':
+    keyboard.go()
